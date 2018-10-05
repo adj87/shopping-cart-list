818e67b4d3c79213df98d94d6fb2d37435a07d77
albreak <Jauregui6>
2018-10-05 20:09:30 +0200

    product-listing component added. Redux implemented but not working yet

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import CartPage from "./pages/CartPage";
+import React from "react"
+import { Switch, Route } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import CartPage from "./pages/CartPage"
 
 const Router = () => (
   <switch>
     <Route exact path="/" component={HomePage} />
     <Route path="/cart" component={CartPage} />
   </switch>
-);
+)
 
-export default Router;
+export default Router
---
+++ src/features/product-listing/product-list-item.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function ProductListItem(props) {
+  console.log(props)
+  return (
+    <div>
+      <h3>{props.product.name}</h3>
+      <img
+        height={100}
+        title={props.product.name}
+        src={`/products/${props.product.image}`}
+      />
+      <div>{props.product.description}</div>
+      <div>{props.product.price}</div>
+      <div>
+        <button>Add to cart</button>
+      </div>
+    </div>
+  )
+}
