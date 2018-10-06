4daf81bbdd353c398656de289976d968e3f07782
albreak <Jauregui6>
2018-10-06 11:02:58 +0200

    cart reducer added

---
+++ src/features/cart/reducer.js
@@ -0,0 +1,10 @@
+const cartReducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD":
+      return [...state, action.payload]
+      break
+    case "REMOVE":
+      const firstMatchIndex = state.indexOf(action.payload)
+      return state.filter((item, index) => index !== firstMatchIndex)
+  }
+}
